9a4283e120df28da1ed6dea249a92954460c3ebe
moeebii <ebiim621@newschool.edu>
2024-03-06 06:35:06 -0500

    r

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let intervalId;
-  
-    document.addEventListener("mousemove", function (event) {
+  let intervalId;
+
+  document.addEventListener("mousemove", function (event) {
+    // Check if the screen width is greater than 768px (adjust as needed)
+    if (window.innerWidth > 768) {
       clearInterval(intervalId);
-  
+
       intervalId = setInterval(() => {
         const redCircle = document.createElement("div");
         redCircle.classList.add("red-circle");
         document.body.appendChild(redCircle);
-  
+
         const mouseX = event.pageX;
         const mouseY = event.pageY;
-  
+
         redCircle.style.left = mouseX + "px";
         redCircle.style.top = mouseY + "px";
-  
+
         setTimeout(() => {
           redCircle.remove();
         }, 2000);
-      }, 200); // Change this value to 500 for every 0.5 seconds
-    });
+      }, 500); // Change this value to 500 for every 0.5 seconds
+    }
   });
-  
+});
